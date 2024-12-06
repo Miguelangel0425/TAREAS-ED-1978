5833c0e1b53b1cc12fdb598e87276b88bb49d482
Alexander Benavides <xavbenav23@gmail.comgit condig --global user.name Alexander>
2024-12-06 16:17:15 -0500

    Agregando carpeta Alexander_Benavides_Deberes

---
+++ Alexander_Benavides_Deberes/Deber 3 EliminarLetra/Eliminar letras/EliminarLetras/Utils.h
@@ -0,0 +1,17 @@
+#pragma once
+#ifndef UTILS_H
+#define UTILS_H
+
+#include <iostream>
+#include <string>
+#include <unordered_map>
+#include <set>
+using namespace std;
+
+// Funciones auxiliares
+string ingresarLetras(const string& mensaje);
+string ingresar(const string& mensaje);
+bool validarCedulaEcuatoriana(const string& cedula);
+
+#endif
+
