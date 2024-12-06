f3724b5c2cc140729ff4a3d2854b4bd887182060
PEREZ CONDOR CARLOS ANDRES <carlospe0505@gmail.com>
2024-12-06 16:13:54 -0500

    Deberes

---
+++ Carlos_Perez_Deberes_1Parcial/Deber 2 Validaciones/Validaciones/MainValidaciones.cpp
@@ -0,0 +1,54 @@
+/***************************************************************************************
+ *            UNIVERSIDAD DE LAS FUERZAS ARMADAS ESPE                                  *
+ * Proposito:                      Validaciones                                        *
+ * Autor:                          Carlos Perez                                        *
+ * Fecha de creacion:              25/11/2024                                          *
+ * Fecha de modificacion:          25/11/2024                                          *
+ * Materia:                        Estructura de datos                                 *
+ * NRC :                           1978                                                *
+ **************************************************************************************/
+
+#include <iostream>
+#include <stdio.h>
+#include <cstdlib>
+#include <conio.h>
+#include <string>
+#include "Validaciones.cpp"
+
+using namespace std;
+
+int main(){
+    
+    int numeroEntero1, numeroEntero2, resultadoEntero;
+    float numeroFlotante1,numeroFlotante2, resultadoFlotante;
+    double numeroDoble1,numeroDoble2, resultadoDoble;
+    char let;
+    string cadena;
+
+    Validaciones<int> entero;
+    Validaciones<float> flotante;
+    Validaciones<string> nombre;
+    Validaciones<double> doble;
+    Validaciones<char> letra;
+
+
+    numeroEntero1 = entero.ingresar("ingrese un valor entero: ", "entero");
+    cout << endl << numeroEntero1 << endl;
+    numeroEntero2 = entero.ingresar("ingrese un valor entero: ", "entero");
+    cout << endl << numeroEntero2 << endl;
+
+
+    numeroFlotante1 = flotante.ingresar("ingrese un valor flotante: ", "flotante");
+    cout << endl << numeroFlotante1 << endl;
+    numeroFlotante2 = flotante.ingresar("ingrese un valor flotante: ", "flotante");
+    cout << endl << numeroFlotante2 << endl;
+
+
+    numeroDoble1 = doble.ingresar("ingrese un valor double: ", "double");
+    cout << endl << numeroDoble1 << endl;
+    let = letra.ingresar("ingrese una letra: ", "char");
+    cout << endl << let << endl;
+    cadena = nombre.ingresar("ingrese su nombre: ", "string");
+    cout << endl << cadena << endl;
+    return 0;
+}
---
+++ Carlos_Perez_Deberes_1Parcial/Deber 3 EliminarCaracter/Utils.h
@@ -0,0 +1,16 @@
+#pragma once
+#ifndef UTILS_H
+#define UTILS_H
+
+#include <iostream>
+#include <string>
+#include <unordered_map>
+#include <set>
+using namespace std;
+
+// Funciones auxiliares
+string ingresarLetras(const string& mensaje);
+string ingresar(const string& mensaje);
+bool validarCedulaEcuatoriana(const string& cedula);
+
+#endif
