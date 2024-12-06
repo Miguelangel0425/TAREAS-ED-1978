ee1f64a34a00d85858cf5f4fe972a0ff5d121e3e
PEREZ CONDOR CARLOS ANDRES <carlospe0505@gmail.com>
2024-12-06 16:09:32 -0500

    Deberes

---
+++ Carlos_Perez_Deberes_1Parcial/Deber 1 Fracciones/Ejercicio 1/Ejercicio 2/Fraccion.cpp
@@ -0,0 +1,27 @@
+// Fraccion.cpp
+#include "Fraccion.h"
+#include <stdexcept>
+
+Fraccion::Fraccion(int num, int denom) : numerador(num), denominador(denom) {
+    if (denominador == 0) {
+        throw std::invalid_argument("El denominador no puede ser cero.");
+    }
+}
+
+int Fraccion::getNumerador() const {
+    return numerador;
+}
+
+int Fraccion::getDenominador() const {
+    return denominador;
+}
+
+float Fraccion::valorDecimal() const {
+    return static_cast<float>(numerador) / denominador;
+}
+
+Fraccion Fraccion::multiplicar(const Fraccion& otra) const {
+    int nuevoNumerador = numerador * otra.getNumerador();
+    int nuevoDenominador = denominador * otra.getDenominador();
+    return Fraccion(nuevoNumerador, nuevoDenominador);
+}
