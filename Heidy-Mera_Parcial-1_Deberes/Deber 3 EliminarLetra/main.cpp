115a8f98f8226f685535e79b0960616441b17d55
hsmera <hsmera@espe.edu.ec>
2024-12-06 16:46:50 -0500

    Deber Parcial 1

---
+++ Heidy-Mera_Parcial-1_Deberes/Deber 3 EliminarLetra/main.cpp
@@ -0,0 +1,70 @@
+/*************************************************************
+**     UNIVERSIDAD DE LAS FUERZAS ARMADAS ESPE		        **
+** Alumno: Heidy Mera                                       **
+** Fecha de Creacion: Lunes, 25 de noviembre de 2024        **
+** Fecha de Modificacion:Viernes, 29 de noviembre de 2024   **
+** Enunciado: Eliminar letra del nombre y apellido          **
+** NRC: 1978                                                **
+**************************************************************/
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include "Utils.h"
+#include "Singly_Linked_List.h"
+
+using namespace std;
+
+enum ListaTipo {
+    LISTA_SIMPLE
+};
+
+int main() {
+    SinglyLinkedList<string> listaSimple;
+
+    vector<string> menuPrincipal = {
+        "Agregar persona (nombre, apellido, cedula)",
+        "Mostrar elementos de la lista",
+        "Salir"
+    };
+
+    while (true) {
+        cout << "Seleccione una opci�n:" << endl;
+        for (size_t i = 0; i < menuPrincipal.size(); ++i) {
+            cout << i + 1 << ". " << menuPrincipal[i] << endl;
+        }
+        cout << "Opci�n: ";
+        int opcion;
+        cin >> opcion;
+        cin.ignore();
+
+        switch (opcion) {
+        case 1: { // Agregar persona
+            cout << "Ingrese los datos de la persona:" << endl;
+
+            string nombre1 = ingresarLetras("Primer nombre");
+            string apellido = ingresarLetras("Apellido");
+            string cedula = ingresar("Ingrese la c�dula");
+
+            if (!validarCedulaEcuatoriana(cedula)) {
+                cerr << "C�dula no v�lida. Intente nuevamente." << endl;
+                break;
+            }
+
+            listaSimple.insertarAlFinal(nombre1 + " " + apellido);
+            cout << "Persona registrada con �xito." << endl;
+            break;
+        }
+        case 2: { // Mostrar elementos
+            cout << "Elementos en la lista:" << endl;
+            listaSimple.mostrar();
+            break;
+        }
+        case 3: // Salir
+            cout << "Saliendo del programa..." << endl;
+            return 0;
+        default:
+            cerr << "Opci�n no v�lida. Intente nuevamente." << endl;
+        }
+    }
+}
