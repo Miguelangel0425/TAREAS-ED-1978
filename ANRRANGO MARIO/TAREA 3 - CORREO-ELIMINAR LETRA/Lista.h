175fc333e29095fd13083fafd98a517783e24950
Mario-Anrrango <mariosanti280704@gmail.com>
2024-12-06 23:05:44 -0500

    DEBER PARCIAL 1

---
+++ ANRRANGO MARIO/TAREA 3 - CORREO-ELIMINAR LETRA/Lista.h
@@ -0,0 +1,20 @@
+#ifndef LISTA_H
+#define LISTA_H
+
+#include "Nodo.h"
+#include <string>
+
+class Lista {
+private:
+    Nodo* cabeza;
+public:
+    Lista();
+    ~Lista();
+
+    void agregarNodo(const std::string& nombre1, const std::string& nombre2, 
+                     const std::string& apellido1, const std::string& apellido2);
+    void eliminarCaracter(char caracter);
+    void imprimirLista();
+};
+
+#endif
